ddfd5ec7ca460cbd407e7fada58b313e1d55b6e0
Beth <Beth3346@gmail.com>
2021-10-27 07:50:33 -0500

    update deps and file structure

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const del = require("del");
+
+var paths = {
+  app: "dist/",
+  css: "dist/css/",
+  images: "dist/images/",
+};
+
+// Clean assets
+function clean() {
+  return del(["./dist/css/", "./dist/images/", "./dist/index.html"]);
+}
+
+// Copy HTML
+function copyIndex() {
+  return gulp.src("./index.html").pipe(gulp.dest(paths.app));
+}
+
+function copyImages() {
+  return gulp
+    .src("./images/**/*.{gif,jpg,png,svg}")
+    .pipe(gulp.dest(paths.images));
+}
+
+gulp.task("default", () => {
+  copyImages();
+
+  return gulp
+    .src("./*.scss")
+    .pipe(plumber())
+    .pipe(
+      sass({
+        includePaths: ["node_modules"],
+      }).on("error", sass.logError)
+    )
+    .pipe(gulp.dest(paths.css))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest(paths.css))
+    .pipe(cleanCSS({ debug: true }))
+    .pipe(gulp.dest(paths.css))
+    .pipe(copyIndex());
+});
+
+exports.clean = clean;
+exports.copyIndex = copyIndex;
+exports.copyImages = copyImages;
